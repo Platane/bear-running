cfa6dcf06f9769221f43fbbc69fbc34bba05dbe8
platane <me@platane.me>
2017-11-23 12:52:37 +0100

    :rocket: [api] deployment script

--- packages/bear-running-api/src/config/index.js
+++ packages/bear-running-api/src/config/index.js
@@ -1,13 +1,9 @@
-export const MONGOLAB_USER = 's634193'
-export const MONGOLAB_PASS = 's634193@mvrht.nets634193@mvrht.net'
-export const MONGOLAB_MAIL = 's634193@mvrht.net'
-
-export const MONGO_DB = 'dasd19f1jd0d13m'
-// export const MONGO_DB = 'dk919djd91jfaod1'
-export const MONGO_USER = 'root'
-export const MONGO_PASS = 'a210kfk09jakn109jfnhga13'
-export const MONGO_ADDRESS = 'ds117156.mlab.com'
-export const MONGO_PORT = '17156'
+// the value by default is the dev db
+export const MONGO_DB = process.env.MONGO_DB || 'dasd19f1jd0d13m'
+export const MONGO_USER = process.env.MONGO_USER || 'root'
+export const MONGO_PASS = process.env.MONGO_PASS || 'no'
+export const MONGO_ADDRESS = process.env.MONGO_ADDRESS || 'ds117156.mlab.com'
+export const MONGO_PORT = process.env.MONGO_PORT || '17156'
 
 export const JWT_PRIVATE_KEY = process.env.JWT_PRIVATE_KEY || '1234'
 export const PORT = process.env.PORT || 8084
