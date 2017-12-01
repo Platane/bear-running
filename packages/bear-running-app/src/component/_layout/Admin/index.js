4144a75ab6ea764d9a0ae9bb01d5b676c3ebfd5c
platane <me@platane.me>
2017-12-01 13:22:54 +0100

    :rocket: [app] styling

--- packages/bear-running-app/src/component/_layout/Admin/index.js
+++ packages/bear-running-app/src/component/_layout/Admin/index.js
@@ -1,6 +1,9 @@
 import { h, Component } from 'preact'
 import { Header } from '~/component/Header'
 import { Link } from '~/component/Link'
+import { Footer } from '~/component/Footer'
+import { grey } from '~/component/_abstract/palette'
+import styled from 'preact-emotion'
 
 const AdminHeader = () => (
   <header>
@@ -9,10 +12,22 @@ const AdminHeader = () => (
 )
 
 export const AdminLayout = ({ children }) => (
-  <div>
+  <Container>
     <Header />
     <AdminHeader />
-    <section>{children}</section>
-    <footer />
-  </div>
+    <Content>{children}</Content>
+    <Footer />
+  </Container>
 )
+
+const Container = styled.div`
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  min-height: 100%;
+`
+const Content = styled.div`
+  flex-grow: 1;
+  flex-basis: 100px;
+  position: relative;
+`
